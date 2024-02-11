6d75e836e43f2dd6413fdf372600e1a1a6ed5c33
Winding-Fly <159670760+Winding-Fly@users.noreply.github.com>
2024-02-11 19:55:43 +0800

    Delete python directory

--- python/data.py
+++
@@ -1,9 +0,0 @@
-class _Data:
-	def __init__(self):
-		self.n=9
-		self.boxsize=3
-		self.path='data.txt'
-		self.t_path='input.txt'
-		self.config='config.txt'
-		self.save_path='result.txt'
-		self.input_class=0
